3c4246e2a57e0be9348425d56ff84022b2c079d0
drake27 <piotrkaczorek@o2.pl>
2018-03-19 16:23:48 +0100

    Po poprawce po pulu 2

--- src/main/java/pl/akademiakodu/mems/repository/CategoryDaoImplements.java
+++ src/main/java/pl/akademiakodu/mems/repository/CategoryDaoImplements.java
@@ -8,28 +8,18 @@ import java.util.List;
 
 @Component
 public class CategoryDaoImplements implements CategoryDao{
-    List<Category> categoryList = new ArrayList<>();
+
     @Override
     public List<Category> findAll() {
+        List<Category> categoryList = new ArrayList<>();
         categoryList.add(new Category(1, "Android"));
         categoryList.add(new Category(2, "Funny"));
         categoryList.add(new Category(3, "Programming"));
         return categoryList;
     }
 
-    public CategoryDaoImplements(List<Category> categoryList) {
-        this.categoryList = categoryList;
-    }
 
     public CategoryDaoImplements(){
-
     }
 
-    public List<Category> getCategoryList() {
-        return categoryList;
-    }
-
-    public void setCategoryList(List<Category> categoryList) {
-        this.categoryList = categoryList;
-    }
 }
