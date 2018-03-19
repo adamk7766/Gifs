d129e1a924c2e8aaba3c2017d2a611f645acf9ca
Tomasz Domagała <tomdom886@gmail.com>
2018-03-19 11:11:25 +0100

    Added files interface GifDAO and Gif

---
+++ src/main/java/pl/akademiakodu/mems/model/Gif.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu.mems.model;
+
+/**
+ * Created by TomDom on 2018-03-19.
+ */
+public class Gif {
+
+    private Long id;
+
+    private String name;
+
+
+}
---
+++ src/main/java/pl/akademiakodu/mems/repository/GifDAO.java
@@ -0,0 +1,8 @@
+package pl.akademiakodu.mems.repository;
+
+/**
+ * Created by TomDom on 2018-03-19.
+ */
+
+public interface GifDAO {
+}
